5ab3d94ad1271bbd70cf38a0e41549d27d85d49b
Jesus Junior <43008669+yisas97@users.noreply.github.com>
2025-06-18 18:27:39 -0500

    Añadir puerto en el blockchain

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
+const express = require('express');
 const Blockchain = require("./src/blockchain");
 const Block = require("./src/block");
 
-async function run() {
-  const blockchain = await new Blockchain();
-  const block1 = new Block({ data: "Block #1" });
-  await blockchain.addBlock(block1);
-  const block2 = new Block({ data: "Block #2" });
-  await blockchain.addBlock(block2);
-  const block3 = new Block({ data: "Block #3" });
-  await blockchain.addBlock(block3);
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-  blockchain.print();
+let blockchainData = '';
+
+async function runBlockchain() {
+  const blockchain = new Blockchain();
+  
+  const block1 = new Block({ data: 'Block #1' });
+  await blockchain.addBlock(block1);
+  
+  const block2 = new Block({ data: 'Block #2' });
+  await blockchain.addBlock(block2);
+  
+  const block3 = new Block({ data: 'Block #3' });
+  await blockchain.addBlock(block3);
+  
+  blockchainData = blockchain.chain.map(block => ({
+    hash: block.hash,
+    height: block.height,
+    body: block.body.toString(),
+    time: block.time,
+    previousBlockHash: block.previousBlockHash
+  }));
 }
 
-run();
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Blockchain running!',
+    blocks: blockchainData
+  });
+});
+
+runBlockchain().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+});
